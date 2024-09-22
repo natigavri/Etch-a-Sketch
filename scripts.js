f4995466a9a6b78735bec2886cf57d3e4343fa1a
Natigavri <nati661@gmail.com>
2024-09-22 16:41:07 +0300

    Add Opacity and Random color
    added on mouseover - incremental opacity change
    and random color change, per square.

--- scripts.js
+++ scripts.js
@@ -11,3 +11,17 @@ for (let i = 0; i < defGrid; i++) {
     }
     grid.appendChild(row);
 }
+
+addEventListener("mouseover", (e) => {
+    if(e.target.parentNode.className === 'row'){
+        const ranColor = () => Number(Math.floor(Math.random() * 256));
+        let opacity = Number(e.target.style.opacity);
+        if(opacity < 1) {
+            opacity += 0.1;
+            e.target.style.opacity = opacity;
+            e.target.style.backgroundColor = `rgba(${ranColor()}, ${ranColor()}, ${ranColor()})`;
+        }else{
+            e.target.style.backgroundColor = `rgba(${ranColor()}, ${ranColor()}, ${ranColor()})`;
+        }
+    }
+})
